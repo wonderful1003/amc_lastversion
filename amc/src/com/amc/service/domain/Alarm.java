3f24c551c09b3d386a36c7317ce7d2f4db9e306a
KO <KO@KO-PC>
2017-10-13 19:28:13 +0900

    z_YJ : Alarm 도메인 필드 수정

--- amc/src/com/amc/service/domain/Alarm.java
+++ amc/src/com/amc/service/domain/Alarm.java
@@ -2,51 +2,70 @@ package com.amc.service.domain;
 
 public class Alarm {
 
-		int openAlarmNo;
+		int alarmNo;
 		int screenContentNo;
 		String userId;
-		String openAlarmRegDate;
+		String alarmRegDate;
+		String alarmSeatNo;
+		String alarmFlag;
 		
 		public Alarm() {
 			super();
 		}
-		
-		public int getOpenAlarmNo() {
-			return openAlarmNo;
+
+		public int getAlarmNo() {
+			return alarmNo;
 		}
-		
-		public void setOpenAlarmNo(int openAlarmNo) {
-			this.openAlarmNo = openAlarmNo;
+
+		public void setAlarmNo(int alarmNo) {
+			this.alarmNo = alarmNo;
 		}
-		
+
 		public int getScreenContentNo() {
 			return screenContentNo;
 		}
-		
+
 		public void setScreenContentNo(int screenContentNo) {
 			this.screenContentNo = screenContentNo;
 		}
-		
+
 		public String getUserId() {
 			return userId;
 		}
-		
+
 		public void setUserId(String userId) {
 			this.userId = userId;
 		}
-		
-		public String getOpenAlarmRegDate() {
-			return openAlarmRegDate;
+
+		public String getAlarmRegDate() {
+			return alarmRegDate;
 		}
-		
-		public void setOpenAlarmRegDate(String openAlarmRegDate) {
-			this.openAlarmRegDate = openAlarmRegDate;
+
+		public void setAlarmRegDate(String alarmRegDate) {
+			this.alarmRegDate = alarmRegDate;
 		}
-		
+
+		public String getAlarmSeatNo() {
+			return alarmSeatNo;
+		}
+
+		public void setAlarmSeatNo(String alarmSeatNo) {
+			this.alarmSeatNo = alarmSeatNo;
+		}
+
+		public String getAlarmFlag() {
+			return alarmFlag;
+		}
+
+		public void setAlarmFlag(String alarmFlag) {
+			this.alarmFlag = alarmFlag;
+		}
+
 		@Override
 		public String toString() {
-			return "OpenAlarm [openAlarmNo=" + openAlarmNo + ", screenContentNo=" + screenContentNo + ", userId="
-					+ userId + ", openAlarmRegDate=" + openAlarmRegDate + "]";
+			return "Alarm [alarmNo=" + alarmNo + ", screenContentNo=" + screenContentNo + ", userId=" + userId
+					+ ", alarmRegDate=" + alarmRegDate + ", alarmSeatNo=" + alarmSeatNo + ", alarmFlag=" + alarmFlag
+					+ "]";
 		}
 		
 }
