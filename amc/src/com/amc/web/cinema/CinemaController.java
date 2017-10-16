4a39900665956586bc212a0b6cbe9762deb01536
KO <KO@KO-PC>
2017-10-16 09:02:11 +0900

    z_YJ :CinemaController.java 생성

--- amc/src/com/amc/web/cinema/CinemaController.java
+++ amc/src/com/amc/web/cinema/CinemaController.java
@@ -1,5 +1,33 @@
 package com.amc.web.cinema;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.amc.common.Search;
+import com.amc.service.cinema.CinemaService;
+
+@Controller
+@RequestMapping("/cinema/*")
 public class CinemaController {
 
+	@Autowired
+	@Qualifier("alarmServiceImpl")
+	private CinemaService clarmService;
+	
+	public CinemaController(){
+		System.out.println("alarmController() default Constructor");
+	}
+	
+	@RequestMapping(value="", method=RequestMethod.GET)
+	public String getCancelAlarmList(@ModelAttribute("Search")Search search, Model model) throws Exception{
+		
+		return null;
+		
+	}
+	
 }
